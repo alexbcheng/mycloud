4a3f7119cd9f450395417942ae9b1191b79b2add
jr_slime <alexbcheng@gmail.com>
2019-08-12 21:27:01 +0000

    20190812a

--- YelpCamp1/app.js
+++ YelpCamp1/app.js
@@ -2,16 +2,39 @@ const 	express = require("express"),
 		app = express(),
 		mongoose = require('mongoose'),
 		bodyParser = require("body-parser"),
+		methodOverride = require("method-override"),
+	  	expressSanitizer = require("express-sanitizer"),
+	  	passport = require("passport"),
+	  	LocalStrategy = require("passport-local"),
+	  	passportLocalMongoose = require("passport-local-mongoose"),
 		port = 3000;
 var rp = require('request-promise'),
 	campsite = require('./models/campground'),
-	Comment = require('./models/comment')
+	Comment = require('./models/comment'),
+	User = require('./models/user')
 	;
 
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+app.use(expressSanitizer());
+app.use(methodOverride("_method")); // what to look for in the URL
+
+app.use(require("express-session")({
+	secret : "hakunamatata",
+	resave : false,
+	saveUninitialized :false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next) {
+	res.locals.currentUser = req.user;
+	next();
+});
 
 mongoose.connect('mongodb+srv://ugatdba:ugatdba01@cluster0-qoqcv.mongodb.net/YelpCamp?retryWrites=true', {
 	useNewUrlParser: true,
@@ -36,7 +59,7 @@ app.get("/campgrounds", function(req, res) {
 		}
 	});
 });
-app.post("/campgrounds", function(req, res) {
+app.post("/campgrounds", isLoggedIn, function(req, res) {
 	//campsites1.push({name: req.body.name, url: req.body.url, image: req.body.image});
 	campsite.create({
 		name: req.body.name,
@@ -51,7 +74,7 @@ app.post("/campgrounds", function(req, res) {
 		}
 	});
 });
-app.get("/campgrounds/new", function(req, res) {
+app.get("/campgrounds/new", isLoggedIn, function(req, res) {
 	res.render("campgrounds/new");
 });
 app.get("/campgrounds/:id", function(req, res) {
@@ -68,13 +91,20 @@ app.get("/campgrounds/:id", function(req, res) {
 // - Comments route
 //
 
-app.post("/campgrounds/:id/comments", function(req, res) {
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res) {
 	campsite.findById(req.params.id, function(err, campground) {
 		if (err) {
 			console.log(err);
 			res.redirect("/campgrounds");
 		} else {
-			Comment.create(req.body.comment, function(err, comment) {
+			//Comment.create(req.body.comment, function(err, comment) {
+			Comment.create({
+				text : req.body.text,
+				author : {
+					id : req.user._id,
+					username : req.user.username
+				}
+			}, function(err, comment) {
 				if (err) {
 					console.log(err);
 				} else {
@@ -88,6 +118,45 @@ app.post("/campgrounds/:id/comments", function(req, res) {
 	});
 });
 
+//
+// - Auth routes
+//
+app.get("/login", function(req, res) {
+	res.render("auth/login");
+});
+app.post("/register", function(req, res) {
+	var newUser = new User({
+		username: req.body.username,
+		description: req.body.description
+	});
+	User.register(newUser, req.body.password, function(err, user) {
+		if (err) {
+			console.log(err);
+			return res.render("login");
+		}
+		passport.authenticate("local")(req, res, function() {
+			res.redirect("/campgrounds");
+		});
+	});
+});
+app.post("/login", passport.authenticate("local", {
+	successRedirect: "/campgrounds",
+	failureRedirect: "/login"
+	}), function(req, res) {
+	
+});
+app.get("/logout", function(req, res) {
+	req.logout();
+	res.redirect("/campgrounds");
+});
+
+function isLoggedIn(req, res, next) {
+	if(req.isAuthenticated()) {
+		return next();
+	}
+	res.redirect("/login");
+}
+
 app.listen(port, () => {
-	console.log('server listing on port '+port);
+	console.log('server started. listening on port '+port);
 });
--- YelpCamp1/models/comment.js
+++ YelpCamp1/models/comment.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 
 const commentSchema = new mongoose.Schema({
 	text: String,
-	author: String
+	author: {
+		id: {type: mongoose.Schema.Types.ObjectId,
+			 ref: "User"
+			},
+		username: String
+	}
 });
 
 // model name here needs to be singular; will be interpreted as plural on the DB collection
